e1fc3a72087da98a6cea5821e00f89ced4ced4cd
Tomoya Nishimura <north.the.star@gmail.com>
2021-01-26 14:38:37 +0900

    アーカイブの完成

--- single.php
+++ single.php
@@ -1,11 +1,10 @@
-<!-- ヘッダーの読み込み -->
 <?php get_header(); ?>
 <?php the_post(); ?>
-<!-- サムネイルの表示 -->
-<?php the_post_thumbnail('category-thumb'); ?>
-<!-- ブログタイトルの表示 -->
-<h1><?php the_title(); ?></h1>
-<!-- 本文の表示 -->
-<p><?php the_content(); ?></p>
-<!-- フッターの読み込み -->
+
+<div class="main-content">
+    <?php the_post_thumbnail('category-thumb'); ?>
+    <h1><?php the_title(); ?></h1>
+    <p><?php the_content(); ?></p>
+</div>
+
 <?php get_footer(); ?>
